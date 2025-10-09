9cb326e1763ccbcfd62a107b9deac09782546827
samelamaria <samelaferreira29@gmail.com>
2025-10-08 22:29:06 -0300

    Adicionando profissional Samela

---
+++ src/SmsfProfissional.java
@@ -0,0 +1,12 @@
+public class SmsfProfissional implements InterfaceProfissional {
+
+    @Override
+    public String suaAreaAtuacao() {
+        return "DevOps/Cloud Engineer";
+    }
+
+    @Override
+    public String seuNome() {
+        return "Samela Maria da Silva Ferreira";
+    }
+}
