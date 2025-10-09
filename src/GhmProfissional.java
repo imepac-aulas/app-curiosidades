6793d585f9775f926948357793cfc761ae616ba7
gugumartins <gustavo.hmartins@aluno.imepac.edu.br>
2025-10-08 22:29:40 -0300

    | minha profissão - Gustavo Henrique Martins |

---
+++ src/GhmProfissional.java
@@ -0,0 +1,9 @@
+public class GhmProfissional implements InterfaceProfissional {
+    @Override
+    public String suaAreaAtuacao() {
+        return "Desenvolvedor de Front-end";
+    }
+    public String seuNome() {
+        return "Gustavo H. Martins";
+    }
+}
--- src/MenuApplication.java
+++ src/MenuApplication.java
@@ -3,14 +3,21 @@ public class MenuApplication {
 
     public static void main(String[] args) {
 
-        System.out.println("Menu de profissões --------------");
+        System.out.println("Menu de profissões \n--------------");
         mostrarEhfProfissional();
         System.out.println("--------------");
+        mostrarGhmProfissional();
     }
 
     public static void mostrarEhfProfissional() {
         EhfProfissional ehfProfissional = new EhfProfissional();
         System.out.println(ehfProfissional.seuNome());
         System.out.println(ehfProfissional.suaAreaAtuacao());
-    }
+            }
+            public static void mostrarGhmProfissional(){
+        GhmProfissional ghmProfissional = new GhmProfissional();
+        System.out.println(ghmProfissional.seuNome());
+        System.out.println(ghmProfissional.suaAreaAtuacao());
+                System.out.println("--------------");
+            }
 }
