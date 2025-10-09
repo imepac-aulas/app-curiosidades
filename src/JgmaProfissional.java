d1aaffd69f553c39f55e9c986c17657f098b10e9
unknown <251-000636@IMEPACAD.local>
2025-10-08 22:29:41 -0300

    minha profissão - João Gabriel Mendonça De Almeida

---
+++ src/JgmaProfissional.java
@@ -0,0 +1,10 @@
+public class JgmaProfissional implements InterfaceProfissional{
+    @Override
+    public String suaAreaAtuacao() {return "DEV";
+    }
+
+    @Override
+    public String seuNome() {
+        return "João Gabriel Mendonça De Almeida";
+    }
+}
--- src/MenuApplication.java
+++ src/MenuApplication.java
@@ -5,6 +5,7 @@ public class MenuApplication {
 
         System.out.println("Menu de profissões --------------");
         mostrarEhfProfissional();
+        mostrarJgmaProfissional();
         System.out.println("--------------");
     }
 
@@ -12,5 +13,11 @@ public class MenuApplication {
         EhfProfissional ehfProfissional = new EhfProfissional();
         System.out.println(ehfProfissional.seuNome());
         System.out.println(ehfProfissional.suaAreaAtuacao());
+    }
+        public static void mostrarJgmaProfissional() {
+            JgmaProfissional JgmaProfissional = new JgmaProfissional();
+            System.out.println(JgmaProfissional.seuNome());
+            System.out.println(JgmaProfissional.suaAreaAtuacao());
+
     }
 }
