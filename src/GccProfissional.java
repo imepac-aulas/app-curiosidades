4895d16bac8033ceaa8c73934467aed3f3cd0381
Gabriel Chaves <gabrielgalaxy66@gmail.com>
2025-10-08 22:31:19 -0300

    minha profissao - Gabriel Cunha Chaves

---
+++ src/GccProfissional.java
@@ -0,0 +1,8 @@
+public class GccProfissional implements InterfaceProfissional{
+
+    @Override
+    public String suaAreaAtuacao() {return "Desenvolvedor de Software"; }
+
+    @Override
+    public String seuNome() {return "Gabriel C. Chaves"; }
+}
--- src/MenuApplication.java
+++ src/MenuApplication.java
@@ -5,12 +5,19 @@ public class MenuApplication {
 
         System.out.println("Menu de profissões --------------");
         mostrarEhfProfissional();
-        System.out.println("--------------");
+        mostrarGccProfissional();
+                System.out.println("--------------");
     }
 
     public static void mostrarEhfProfissional() {
         EhfProfissional ehfProfissional = new EhfProfissional();
         System.out.println(ehfProfissional.seuNome());
         System.out.println(ehfProfissional.suaAreaAtuacao());
+        System.out.println();
+    }
+    public static void mostrarGccProfissional(){
+        GccProfissional gccProfissional = new GccProfissional();
+        System.out.println(gccProfissional.seuNome());
+        System.out.println(gccProfissional.suaAreaAtuacao());
     }
 }
