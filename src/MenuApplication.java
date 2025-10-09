a69256c1b7fae353378f1bc3a7e5c82358f64dda
juliapdea <maria.pjulia@aluno.imepac.edu.br>
2025-10-08 22:29:37 -0300

    Adicionando Profissional Maria Julia

--- src/MenuApplication.java
+++ src/MenuApplication.java
@@ -1,16 +1,22 @@
+import java.sql.SQLOutput;
+
 public class MenuApplication {
-
-
     public static void main(String[] args) {
 
         System.out.println("Menu de profissões --------------");
         mostrarEhfProfissional();
+        mostrarMjProfissional();
         System.out.println("--------------");
     }
-
     public static void mostrarEhfProfissional() {
         EhfProfissional ehfProfissional = new EhfProfissional();
         System.out.println(ehfProfissional.seuNome());
         System.out.println(ehfProfissional.suaAreaAtuacao());
+    }
+
+    public static void mostrarMjProfissional() {
+        MjProfissional mjProfissional = new MjProfissional();
+        System.out.println(mjProfissional.seuNome());
+        System.out.println(mjProfissional.suaAreaAtuacao());
     }
 }
---
+++ src/MjProfissional.java
@@ -0,0 +1,12 @@
+public class MjProfissional implements InterfaceProfissional {
+
+    @Override
+    public String suaAreaAtuacao() {
+        return "FrontEnd";
+    }
+
+    @Override
+    public String seuNome() {
+        return "Maria Júlia Pereira de Andrade";
+    }
+}
